503169f3c3a748c00333e2f1cb1d9279dcf8e79d
Aaron Beitch <aaronb@arista.com>
2022-12-01 18:42:52 -0800

    day01: Playing with itertools

--- day01/a.py
+++ day01/a.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python3
 
 import sys
+import itertools
 
 
-f = open(sys.argv[1])
-v = [[]]
-cur = v[0]
-for line in f:
-    line = line.strip()
-    if line == "":
-        cur = []
-        v.append(cur)
-        continue
-    cur.append(int(line))
+def main():
+    f = open(sys.argv[1])
+    it = (line.strip() for line in f)
 
-summed = [sum(series) for series in v]
-print(max(summed))
+    emptyLineCount = 0
+
+    def emptyLineCounter(k: str):
+        nonlocal emptyLineCount
+        if k == "":
+            emptyLineCount += 1
+        return emptyLineCount
+
+    sums = [
+        sum(int(l) for l in g if l != "")
+        for _, g in itertools.groupby(it, emptyLineCounter)
+    ]
+
+    print(max(sums))
+
+
+if __name__ == "__main__":
+    main()
